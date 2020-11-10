6f0952f2b214d146d48fda84553098979573d228
Nabeel Anjum <nabeelanjum888@gmail.com>
2020-11-10 19:27:07 +0500

    Dummy data removed and empty keys handled

--- src/screens/ProductDetails.js
+++ src/screens/ProductDetails.js
@@ -4,54 +4,59 @@ import { Button, Text } from '../components';
 import colors from '../utils/colors';
 import { normalizeX, normalizeY } from '../utils/functions';
 
-const ProductDetails = () => {
+const ProductDetails = ({ route }) => {
 
-  // const { details } = props.route.params;
+  const { details } = route.params;
 
-  const details = {
-    images: ['https://images.barcodelookup.com/4859/48596913-1.jpg'],
-    brand: 'Vasline',
-    category: 'Health & Beauty > Personal Care',
-    description: 'convenience bottle 3.5oz can be carried anywhere',
-    features: [
-      "contain micro-droplets of vaseline jelly",
-      "rich and heavy",
-      "non greasy lotion",
-      "clinicaly proven to restor very dry skin in 5 days"
-    ],
-    manufacturer: 'Unilever',
-    label: 'Unilever',
-    product_name: 'Vaseline Intensive Care Lotion Advanced Strengh 100gr=3.5oz Pack of 6 plus one free Panten Pro-v Deep Fortifying Treament 1.7oz'
-  };
-
-  const { images, brand, category, description, features, manufacturer, label, product_name } = details;
+  const { images, description, category, brand, label, features, manufacturer, product_name } = details;
 
   return (
     <ScrollView style={styles.container}>
       {images.length > 0 && <Image style={styles.productImage} source={{ uri: details.images[0] }} />}
+
       <View style={styles.detailsBody}>
 
         <Text numberOfLines={2} boldest size={16}>{product_name}</Text>
-        <Text color={colors.LIGHT_TEXT}>{category}</Text>
+        {category != 0 && <Text color={colors.LIGHT_TEXT}>{category}</Text>}
 
-        <View style={styles.productDetailsSection}>
-          <Text bolder>Description:</Text>
-          <Text style={styles.detailsText}>{description}</Text>
-        </View>
+        {description != 0 &&
+          <View style={styles.productDetailsSection}>
+            <Text bolder>Description:</Text>
+            <Text style={styles.detailsText}>{description}</Text>
+          </View>
+        }
 
-        <View style={styles.productDetailsSection}>
-          <Text bolder>Manufacturer:</Text>
-          <Text style={styles.detailsText}>{manufacturer}</Text>
-        </View>
+        {manufacturer != 0 &&
+          <View style={styles.productDetailsSection}>
+            <Text bolder>Manufacturer:</Text>
+            <Text style={styles.detailsText}>{manufacturer}</Text>
+          </View>
+        }
 
-        <View style={styles.productDetailsSection}>
-          <Text bolder>Product Details:</Text>
-          {features.map((feature, i) =>
-            <View style={{ marginLeft: 8 }}>
-              <Text style={styles.detailsText} key={i}>→ {feature}</Text>
-            </View>
-          )}
-        </View>
+        {brand != 0 &&
+          <View style={styles.productDetailsSection}>
+            <Text bolder>Brand:</Text>
+            <Text style={styles.detailsText}>{brand}</Text>
+          </View>
+        }
+
+        {label != 0 &&
+          <View style={styles.productDetailsSection}>
+            <Text bolder>Label:</Text>
+            <Text style={styles.detailsText}>{label}</Text>
+          </View>
+        }
+
+        {(features.length > 0) &&
+          <View style={styles.productDetailsSection}>
+            <Text bolder>Product Details:</Text>
+            {features.map((feature, i) =>
+              <View key={i} style={{ marginLeft: 8 }}>
+                <Text style={styles.detailsText}>→ {feature}</Text>
+              </View>
+            )}
+          </View>
+        }
 
       </View>
     </ScrollView>
@@ -64,7 +69,7 @@ const styles = StyleSheet.create({
   },
   productImage: {
     width: '100%',
-    height: normalizeY(150),
+    height: normalizeY(180),
     resizeMode: 'contain',
     backgroundColor: colors.LIGHT_GREY
   },
